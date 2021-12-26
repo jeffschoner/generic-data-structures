b887defba525be3033985184f664cffdb8216e6a
Jeff Schoner <jeffschoner@gmail.com>
2021-12-26 11:15:04 -0800

    Basic set data structure implementation

---
+++ set/set.go
@@ -0,0 +1,98 @@
+package set
+
+type Set[T comparable] struct {
+	storage map[T]struct{}
+}
+
+// Create a new generic-typed set. T must be comparable. You need to use this function
+// to create new sets. Set[int]{} will not work.
+func New[T comparable]() Set[T] {
+	return Set[T]{
+		storage: make(map[T]struct{}),
+	}
+}
+
+// Add an item to the set. Has no effect if the item already exists.
+func (r Set[T]) Add(item T) {
+	r.storage[item] = struct{}{}
+}
+
+// Returns true if the set contains the specified item.
+func (r Set[T]) Contains(item T) bool {
+	_, has := r.storage[item]
+	return has
+}
+
+// Removes the specified item from the set. Has no effect if the item is not present.
+func (r Set[T]) Remove(item T) {
+	delete(r.storage, item)
+}
+
+// Returns the number of items in the set.
+func (r Set[T]) Size() int {
+	return len(r.storage)
+}
+
+// Adds all items from the specified set to this one. This function mutates the set.
+func (r Set[T]) Merge(s Set[T]) {
+	for k := range s.storage {
+		r.Add(k)
+	}
+}
+
+// Creates a new set from all items in this one and the one specified. It is
+// non-destructive to both sets involved.
+func (r Set[T]) Union(s Set[T]) Set[T] {
+	newSet := New[T]()
+
+	for k := range r.storage {
+		newSet.Add(k)
+	}
+
+	for k := range s.storage {
+		newSet.Add(k)
+	}
+	
+	return newSet
+}
+
+// Creates a new set with all items common to this one and the one specified. It is
+// non-destructive to both sets involved.
+func (r Set[T]) Intersection(s Set[T]) Set[T] {
+	newSet := New[T]()
+	
+	var shorterSet *Set[T]
+	var longerSet *Set[T]
+
+	if (r.Size() > s.Size()) {
+		shorterSet = &s
+		longerSet = &r
+	} else {
+		shorterSet = &r
+		longerSet = &s
+	}
+
+	for k := range shorterSet.storage {
+		if longerSet.Contains(k) {
+			newSet.Add(k)
+		}
+	}
+	
+	return newSet
+}
+
+// Creates a new set with the items in this one less any of the items in the specified
+// set. It is non-destructive to both sets involved.
+func (r Set[T]) Difference(s Set[T]) Set[T] {
+	newSet := New[T]()
+
+	for k := range r.storage {
+		newSet.Add(k)
+	}
+
+	for k := range s.storage {
+		newSet.Remove(k)
+	}
+	
+	return newSet
+}
---
+++ set/set_test.go
@@ -0,0 +1,167 @@
+package set
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyInt(t *testing.T) {
+	s := New[int]()
+	assert.Equal(t, 0, s.Size())
+}
+
+func TestAddInt(t *testing.T) {
+	s := New[int]()
+	s.Add(23)
+	s.Add(-22)
+	s.Add(0)
+	assert.Equal(t, 3, s.Size())
+	assert.True(t, s.Contains(23))
+	assert.True(t, s.Contains(-22))
+	assert.True(t, s.Contains(0))
+}
+
+func TestAddDupeInt(t *testing.T) {
+	s := New[int]()
+	s.Add(23)
+	s.Add(0)
+	s.Add(23)
+	assert.Equal(t, 2, s.Size())
+	assert.True(t, s.Contains(23))
+	assert.True(t, s.Contains(0))
+}
+
+func TestRemoveInt(t *testing.T) {
+	s := New[int]()
+	s.Add(23)
+	s.Add(-22)
+	s.Add(0)
+	s.Remove(0)
+	assert.Equal(t, 2, s.Size())
+	assert.True(t, s.Contains(23))
+	assert.True(t, s.Contains(-22))
+}
+
+func TestRemoveUnknownInt(t *testing.T) {
+	s := New[int]()
+	s.Add(23)
+	s.Remove(12)
+	assert.Equal(t, 1, s.Size())
+	assert.True(t, s.Contains(23))
+	assert.False(t, s.Contains(12))
+}
+
+func TestString(t *testing.T) {
+	s := New[string]()
+	s.Add("hello")
+	s.Add("foo")
+	assert.Equal(t, 2, s.Size())
+	assert.True(t, s.Contains("hello"))
+	assert.True(t, s.Contains("foo"))
+
+	s.Remove("unknown")
+	assert.Equal(t, 2, s.Size())
+	assert.False(t, s.Contains("unknown"))
+}
+
+func TestMerge(t *testing.T) {
+	s1 := New[string]()
+	s1.Add("hi")
+	s1.Add("foo")
+
+	s2 := New[string]()
+	s2.Add("bar")
+	s2.Add("hi")
+	s2.Add("baz")
+
+	s1.Merge(s2)
+
+	assert.Equal(t, 3, s2.Size())
+
+	assert.Equal(t, 4, s1.Size())
+	assert.True(t, s1.Contains("foo"))
+	assert.True(t, s1.Contains("bar"))
+	assert.True(t, s1.Contains("hi"))
+	assert.True(t, s1.Contains("baz"))
+}
+
+func TestUnion(t *testing.T) {
+	s1 := New[string]()
+	s1.Add("hi")
+	s1.Add("foo")
+
+	s2 := New[string]()
+	s2.Add("bar")
+	s2.Add("hi")
+	s2.Add("baz")
+
+	s3 := s1.Union(s2)
+
+	assert.Equal(t, 2, s1.Size())
+	assert.Equal(t, 3, s2.Size())
+
+	assert.Equal(t, 4, s3.Size())
+	assert.True(t, s3.Contains("foo"))
+	assert.True(t, s3.Contains("bar"))
+	assert.True(t, s3.Contains("hi"))
+	assert.True(t, s3.Contains("baz"))
+}
+
+func TestIntersection(t *testing.T) {
+	s1 := New[string]()
+	s1.Add("hi")
+	s1.Add("foo")
+
+	s2 := New[string]()
+	s2.Add("bar")
+	s2.Add("hi")
+	s2.Add("baz")
+
+	s3 := s1.Intersection(s2)
+
+	assert.Equal(t, 2, s1.Size())
+	assert.Equal(t, 3, s2.Size())
+
+	assert.Equal(t, 1, s3.Size())
+	assert.True(t, s3.Contains("hi"))
+}
+
+func TestIntersectionFlipLong(t *testing.T) {
+	s1 := New[string]()
+	s1.Add("hi")
+	s1.Add("foo")
+	s1.Add("baz")
+
+	s2 := New[string]()
+	s2.Add("bar")
+	s2.Add("hi")
+
+	s3 := s1.Intersection(s2)
+
+	assert.Equal(t, 3, s1.Size())
+	assert.Equal(t, 2, s2.Size())
+
+	assert.Equal(t, 1, s3.Size())
+	assert.True(t, s3.Contains("hi"))
+}
+
+func TestDifference(t *testing.T) {
+	s1 := New[string]()
+	s1.Add("hi")
+	s1.Add("foo")
+
+	s2 := New[string]()
+	s2.Add("bar")
+	s2.Add("hi")
+	s2.Add("baz")
+
+	s3 := s2.Difference(s1)
+
+	assert.Equal(t, 2, s1.Size())
+	assert.Equal(t, 3, s2.Size())
+
+	assert.Equal(t, 2, s3.Size())
+	assert.True(t, s3.Contains("bar"))
+	assert.True(t, s3.Contains("baz"))
+}
